e60ac7b9aa8906bf9c603fe9548c449c7b5d9ca9
Overseer <104671705+MasterLeeeeeeeee33887@users.noreply.github.com>
2025-08-01 17:29:04 +0800

    Add files via upload

---
+++ Chapter-2/2.16-Displaying-Values-with-printf.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//a) Using one printf statement with no conversion specifications.
+int main(void)
+{
+	printf("1 2 3 4");
+	return 0;
+}
+
+//b) Using one printf statement with four conversion specifications.
+/*
+int main(void)
+{
+	int a = 1, b = 2, c = 3, d = 4;
+	printf("%d %d %d %d", a, b, c, d);
+	return 0;
+}
+*/
+
+
+//c) Using four printf statements.
+/*
+int main(void)
+{
+	printf("1 ");
+	printf("2 ");
+	printf("3 ");
+	printf("4");
+	return 0;
+}
+*/
