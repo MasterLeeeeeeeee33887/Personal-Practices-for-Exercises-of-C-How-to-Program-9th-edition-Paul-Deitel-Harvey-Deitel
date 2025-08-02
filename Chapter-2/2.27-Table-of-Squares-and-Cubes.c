74197f8a35f1eb8d6ead50b3e0ac0c9c03299266
Overseer <104671705+MasterLeeeeeeeee33887@users.noreply.github.com>
2025-08-02 21:22:43 +0800

    Add files via upload

---
+++ Chapter-2/2.27-Table-of-Squares-and-Cubes.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s", "number\tsquare\tcube\n");
+	int a = 0;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	a++;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	a++;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	a++;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	a++;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	a++;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	a++;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	a++;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	a++;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	a++;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	a++;
+	printf("%d\t%d\t%d\n", a, a * a, a * a * a);
+	return 0;
+}
