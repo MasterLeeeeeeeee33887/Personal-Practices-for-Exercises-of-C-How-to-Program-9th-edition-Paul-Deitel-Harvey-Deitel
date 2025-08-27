f9797dc8f0c454b5119477d34413b07b84b6cd79
Overseer <104671705+MasterLeeeeeeeee33887@users.noreply.github.com>
2025-08-27 15:34:07 +0800

    Add files via upload

---
+++ Chapter-4/4.30-Replacing-switch-with-if-else.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+
+/*
+int main(void) {
+    int aCount = 0;
+    int bCount = 0;
+    int cCount = 0;
+    int dCount = 0;
+    int fCount = 0;
+
+    puts("Enter the letter grades.");
+    puts("Enter the EOF character to end input.");
+    int grade = 0; 
+
+
+    while ((grade = getchar()) != EOF) {
+
+        if (grade == 'A' || grade == 'a') {
+            ++aCount;
+        }
+        else if (grade == 'B' || grade == 'b') {
+            ++bCount;
+        }
+        else if (grade == 'C' || grade == 'c') {
+            ++cCount;
+        }
+        else if (grade == 'D' || grade == 'd') {
+            ++dCount;
+        }
+        else if (grade == 'F' || grade == 'f') {
+            ++fCount;
+        }
+        else if (grade == '\n' || grade == '\t' || grade == ' ') {
+            
+        }
+        else{
+            printf("%s", "Incorrect letter grade entered.");
+            puts(" Enter a new grade.");
+        }
+    }
+
+    puts("\nTotals for each letter grade are:");
+    printf("A: %d\n", aCount);
+    printf("B: %d\n", bCount);
+    printf("C: %d\n", cCount);
+    printf("D: %d\n", dCount);
+    printf("F: %d\n", fCount);
+}
+*/
+
+int main(void) {
+    int aCount = 0;
+    int bCount = 0;
+    int cCount = 0;
+    int dCount = 0;
+    int fCount = 0;
+
+    puts("Enter the letter grades.");
+    puts("Enter the EOF character to end input.");
+    int grade = 0;
+
+
+    while ((grade = getchar()) != EOF) {
+
+        if (grade == 'A' || grade == 'a') {
+            ++aCount;
+            continue;
+        }
+        if (grade == 'B' || grade == 'b') {
+            ++bCount;
+            continue;
+        }
+        if (grade == 'C' || grade == 'c') {
+            ++cCount;
+            continue;
+        }
+        if (grade == 'D' || grade == 'd') {
+            ++dCount;
+            continue;
+        }
+        if (grade == 'F' || grade == 'f') {
+            ++fCount;
+            continue;
+        }
+        if (grade == '\n' || grade == '\t' || grade == ' ') {
+            continue;
+        }
+            printf("%s", "Incorrect letter grade entered.");
+            puts(" Enter a new grade.");
+
+    }
+
+    puts("\nTotals for each letter grade are:");
+    printf("A: %d\n", aCount);
+    printf("B: %d\n", bCount);
+    printf("C: %d\n", cCount);
+    printf("D: %d\n", dCount);
+    printf("F: %d\n", fCount);
+}
