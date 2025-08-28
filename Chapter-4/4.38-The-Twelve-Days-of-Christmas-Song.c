7cf0581e6a8c0bed6ce65432f479975fe9e29c2a
Overseer <104671705+MasterLeeeeeeeee33887@users.noreply.github.com>
2025-08-28 14:11:04 +0800

    Add files via upload

---
+++ Chapter-4/4.38-The-Twelve-Days-of-Christmas-Song.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+int main(void)
+{
+	while(1){
+		int x = 1;
+		scanf("%d", &x);
+		if (x == -1) {
+			break;
+		}
+		switch (x) {
+		case 1: 
+			printf("%s", "On the first day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 2:
+			printf("%s", "On the second day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 3:
+			printf("%s", "On the third day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 4:
+			printf("%s", "On the fourth day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 5:
+			printf("%s", "On the fifth day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 6:
+			printf("%s", "On the sixth day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 7:
+			printf("%s", "On the seventh day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 8:
+			printf("%s", "On the eighth day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 9:
+			printf("%s", "On the ninth day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 10:
+			printf("%s", "On the tenth day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 11:
+			printf("%s", "On the eleventh day of Christmas, \nmy true love sent to me\n");
+			break;
+		case 12:
+			printf("%s", "On the twelveth day of Christmas, \nmy true love sent to me\n");
+			break;
+		}
+		switch (x) {
+		case 1:
+			printf("%s", "A partridge in a pear tree.\n\n");
+			break;
+		case 2:
+			printf("%s", "Two turtle doves,\nAnd a partridge in a pear tree.\n\n");
+			break;
+		case 3:
+			printf("%s", "Three French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.\n\n");
+			break;
+		case 4:
+			printf("%s", "Four calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.\n\n");
+			break;
+		case 5:
+			printf("%s", "Five gold rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.\n\n");
+			break;
+		case 6:
+			printf("%s", "Six geese a-laying,\nFive gold rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.\n\n");
+			break;
+		case 7:
+			printf("%s", "Seven swans a-swimming,\nSix geese a-laying,\nFive gold rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.\n\n");
+			break;
+		case 8:
+			printf("%s", "Eight maids a-milking,\nSeven swans a-swimming,\nSix geese a-laying,\nFive gold rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.\n\n");
+			break;
+		case 9:
+			printf("%s", "Nine ladies dancing,\nEight maids a-milking,\nSeven swans a-swimming,\nSix geese a-laying,\nFive gold rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.\n\n");
+			break;
+		case 10:
+			printf("%s", "Ten lords a-leaping,\nNine ladies dancing,\nEight maids a-milking,\nSeven swans a-swimming,\nSix geese a-laying,\nFive gold rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.\n\n");
+			break;
+		case 11:
+			printf("%s", "Eleven pipers piping,\nTen lords a-leaping,\nNine ladies dancing,\nEight maids a-milking,\nSeven swans a-swimming,\nSix geese a-laying,\nFive gold rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree.\n\n");
+			break;
+		case 12:
+			printf("%s", "Twelve drummers drumming,\nEleven pipers piping,\nTen lords a-leaping,\nNine ladies dancing,\nEight maids a-milking,\nSeven swans a-swimming,\nSix geese a-laying,\nFive gold rings,\nFour calling birds,\nThree French hens,\nTwo turtle doves,\nAnd a partridge in a pear tree!\n\n");
+			break;
+		}
+	}
+	return 0;
+}
