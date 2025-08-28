9db6e9cd9c2aa0148ec947666c2982a9dbe1b72e
Overseer <104671705+MasterLeeeeeeeee33887@users.noreply.github.com>
2025-08-28 14:48:17 +0800

    Update 4.40-World-Population-Growth.c

--- Chapter-4/4.40-World-Population-Growth.c
+++ Chapter-4/4.40-World-Population-Growth.c
@@ -7,13 +7,19 @@ int main(void)
 	long long year = 1, population = 8243167084;
 	double rate = 0.0085;
 	long long increase = (double)population * rate;
+	int doubling_year = 0;
 	printf("%s%21s%21s\n", "Year", "Population", "Increase");
 	for (; year <= 75; year++) {
 		printf("%d%21lld%21lld\n", year, population, increase);
 		population += increase;
 		increase = (double)population * rate;
-	}
 
+
+		if (population >= 2 * 8243167084 && doubling_year == 0) {
+			doubling_year = year;
+		}
+	}
+	printf("\nThe doubling year is at the %dth year", doubling_year);
 
 	return 0;
 }
