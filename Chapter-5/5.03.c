695305605266e1a1c80f87debdac9aba2026728b
Overseer <104671705+MasterLeeeeeeeee33887@users.noreply.github.com>
2025-09-23 15:49:56 +0800

    Add files via upload

---
+++ Chapter-5/5.03.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	printf("sqrt(900.0)=%f\n", sqrt(900.0));
+	printf("sqrt(9.0)=%f\n", sqrt(9.0));
+	printf("cbrt(27.0)=%f\n", cbrt(27.0));
+	printf("cbrt(-8.0)=%f\n", cbrt(-8.0));
+	printf("exp(1.0)=%f\n", exp(1.0));
+	printf("exp(2.0)=%f\n", exp(2.0));
+	printf("log(2.718282)=%f\n", log(2.718282));
+	printf("log(7.389056)=%f\n", log(7.389056));
+	printf("log10(1.0)=%f\n", log10(1.0));
+	printf("log10(10.0)=%f\n", log10(10.0));
+	printf("log10(100.0)=%f\n", log10(100.0));
+	printf("fabs(13.5)=%f\n", fabs(13.5));
+	printf("fabs(0.0)=%f\n", fabs(0.0));
+	printf("fabs(-13.5)=%f\n", fabs(-13.5));
+	printf("ceil(9.2)=%f\n", ceil(9.2));
+	printf("ceil(-9.8)=%f\n", ceil(-9.8));
+	printf("floor(9.2)=%f\n", floor(9.2));
+	printf("floor(-9.8)=%f\n", floor(-9.8));
+	printf("pow(2, 7)=%f\n", pow(2, 7));
+	printf("pow(9, .5)=%f\n", pow(9, .5));
+	printf("fmod(13.657, 2.333)=%f\n", fmod(13.657, 2.333));
+	printf("sin(0.0)=%f\n", sin(0.0));
+	printf("cos(0.0)=%f\n", cos(0.0));
+	printf("tan(0.0)=%f\n", tan(0.0));
+
+
+	return 0;
+}
