4df4c343345f985e38471e6b24b74dde0f8839ca
Overseer <104671705+MasterLeeeeeeeee33887@users.noreply.github.com>
2025-10-08 19:27:56 +0800

    Update 5.27-Prime-Numbers.c

--- Chapter-5/5.27-Prime-Numbers.c
+++ Chapter-5/5.27-Prime-Numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 
 
@@ -19,16 +20,16 @@ void isPrime(int a) {
 	int num_of_fac = 0;
 	printf("%s", "Factors for ");
 
-	printf("%d: \n", a);
+	printf("%d until sqrt(%d): \n", a, a);
 
-	for (int x = 1; x <= a; x++) {
+	for (int x = 1; x <sqrt(a); x++) {
 		if (a % x == 0) {
 			printf("%d ", x);
 			num_of_fac++;
 		}
 	}
 	printf("%s", "\n\n");
-	if (num_of_fac == 2) {
+	if (num_of_fac - 1 == 0) {
 		printf("*******The Prime number: %d\n\n", a);
 	}
 }
