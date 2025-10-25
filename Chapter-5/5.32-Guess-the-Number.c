bbbaf8bd3bc7d08397fad113a9e733dfccc6fab3
Overseer <104671705+MasterLeeeeeeeee33887@users.noreply.github.com>
2025-10-25 15:48:45 +0800

    Add files via upload

---
+++ Chapter-5/5.32-Guess-the-Number.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+
+int main(void)
+{
+	srand((unsigned int)time(NULL));
+	printf("%s\n%s\n%s\n", "I have a number between 1 and 1000.", "Can you guess my number?", "Please type your first guess.");
+	int guess = 0, flag = 1, num = rand() % 1000 + 1, count = 1;
+	char c = 0;
+
+	do {
+		scanf("%d", &guess);
+		printf("%d%s", count, ". ");
+		count++;
+		if (guess == num) {
+			printf("%s", "Excellent! You guessed the number!\nWould you like to play again (y or n)?");
+			scanf(" %c", &c);
+			if (c == 'y') {
+				num = rand() % 1000 + 1;
+				printf("\n%s\n%s\n%s\n", "I have a number between 1 and 1000.", "Can you guess my number?", "Please type your first guess.");
+
+			}
+			else {
+				break;
+				return 0;
+			}
+		}
+		else if (guess < num) {
+			printf("%s\n", "Too low. Try again.");
+		}
+		else {
+			printf("%s\n", "Too high. Try again.");
+		}
+
+	} while (1);
+
+	return 0;
+}
